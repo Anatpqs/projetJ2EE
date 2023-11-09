0e150c0f979abe27bd467c891f508f726cd37bd6
Romcast <129684627+Romcast@users.noreply.github.com>
2023-11-09 13:38:44 +0100

    Add files via upload

---
+++ JEESite/src/main/java/Controller/FrontController.java
@@ -0,0 +1,19 @@
+package Controller;
+
+import java.io.*;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+@WebServlet(name = "FrontController", value = "/FrontController")
+public class FrontController extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
