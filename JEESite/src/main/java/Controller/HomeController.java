f21ac2091d9d9b0f697fc1159eb751f880fcd7c3
Romcast <129684627+Romcast@users.noreply.github.com>
2023-12-05 19:01:44 +0100

    Add files via upload

--- JEESite/src/main/java/Controller/HomeController.java
+++ JEESite/src/main/java/Controller/HomeController.java
@@ -1,8 +1,11 @@
 package Controller;
 
 import java.io.*;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
+import dao.CategoryDAO;
 import dao.ProductDAO;
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
@@ -14,16 +17,26 @@ import org.hibernate.Session;
 @WebServlet(name = "HomeController", value = "/HomeController")
 public class HomeController extends HttpServlet {
     private static final int n = 2;
+    private static final int l = 3;
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         ProductDAO dao = new ProductDAO();
-        List<Product> listM = dao.getRandomProductsCategory(n,"M");
-        List<Product> listW = dao.getRandomProductsCategory(n,"W");
-        List<Product> listK = dao.getRandomProductsCategory(n,"K");
+        List<Integer> listC = dao.getRandomCategory(l);
+        int c1 = listC.get(0);
+        int c2 = listC.get(1);
+        int c3 = listC.get(2);
+        List<Product> list1 = dao.getRandomProductsCategory(n,c1);
+        List<Product> list2 = dao.getRandomProductsCategory(n,c2);
+        List<Product> list3 = dao.getRandomProductsCategory(n,c3);
 
-        request.setAttribute("listM", listM);
-        request.setAttribute("listW", listW);
-        request.setAttribute("listK", listK);
+        request.setAttribute("list1", list1);
+        request.setAttribute("list2", list2);
+        request.setAttribute("list3", list3);
+
+        List<Category> list_category= new ArrayList<Category>();
+        list_category=new CategoryDAO().getAllCategories();
+        request.setAttribute("list_category",list_category);
 
         RequestDispatcher dispatcher = request.getRequestDispatcher("home.jsp");
         dispatcher.forward(request, response);
@@ -34,3 +47,4 @@ public class HomeController extends HttpServlet {
 
     }
 }
+
