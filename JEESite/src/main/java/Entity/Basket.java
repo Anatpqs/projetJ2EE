5891c00bcff88691a8e4e5040198862aa960d00a
Anatpqs <120056597+Anatpqs@users.noreply.github.com>
2023-12-10 20:54:27 +0100

    Update Basket.java

--- JEESite/src/main/java/Entity/Basket.java
+++ JEESite/src/main/java/Entity/Basket.java
@@ -4,16 +4,27 @@ import jakarta.persistence.*;
 
 @Entity
 public class Basket {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @Column(name = "idBasket", nullable = false)
+    private int idBasket;
     @Basic
     @Column(name = "idProduct", nullable = false)
     private int idProduct;
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Id
+    @Basic
     @Column(name = "idUser", nullable = false)
     private int idUser;
     @Basic
     @Column(name = "quantity", nullable = false)
     private int quantity;
+
+    public int getIdBasket() {
+        return idBasket;
+    }
+
+    public void setIdBasket(int idBasket) {
+        this.idBasket = idBasket;
+    }
 
     public int getIdProduct() {
         return idProduct;
