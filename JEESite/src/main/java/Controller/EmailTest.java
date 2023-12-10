11b4d2985aa055713f3162443be4f276c500fd1a
Anatpqs <120056597+Anatpqs@users.noreply.github.com>
2023-12-10 20:53:03 +0100

    Update EmailTest.java

--- JEESite/src/main/java/Controller/EmailTest.java
+++ JEESite/src/main/java/Controller/EmailTest.java
@@ -11,7 +11,7 @@ public class EmailTest {
     final String username = "j2ee.mail.confirm@gmail.com"; // Remplacez par votre adresse e-mail
     final String password = "vsca zhln bqvm atvt"; // Remplacez par votre mot de passe
 
-    public void sendConfirmationEmail(String recipientEmail, String confirmationLink) {
+    public void sendConfirmationEmail(String recipientEmail, String subject, String text) {
         Properties props = new Properties();
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.starttls.enable", "true");
@@ -30,8 +30,8 @@ public class EmailTest {
             Message message = new MimeMessage(session);
             message.setFrom(new InternetAddress(username));
             message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(recipientEmail));
-            message.setSubject("Confirmation de votre compte");
-            message.setText("Bienvenue sur notre site! Pour confirmer votre compte, veuillez cliquer sur le lien suivant:\n\n" + confirmationLink);
+            message.setSubject(subject);
+            message.setText(text);
 
             Transport.send(message);
 
